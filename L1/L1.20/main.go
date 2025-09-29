9ed876ead9e39328a358af872de07aaefdbec8f4
daliatoo <daliatoo@student.21-school.ru>
2025-09-29 17:50:13 +0300

    add: commentary

--- L1/L1.20/main.go
+++ L1/L1.20/main.go
@@ -6,15 +6,15 @@ import (
 )
 
 func wordRiverse(str string) string {
-	arrStr := strings.Fields(str)
+	arrStr := strings.Fields(str) //разделяем стрингу на чанки
 	n := len(arrStr) - 1
-	for i, j := 0, n; i < j; i, j = i+1, j-1 {
+	for i, j := 0, n; i < j; i, j = i+1, j-1 { //меняем местами слова
 		arrStr[i], arrStr[n-i] = arrStr[n-i], arrStr[i]
 	}
 	var newString string
 
 	for i := range arrStr {
-		newString += arrStr[i] + " "
+		newString += arrStr[i] + " " //переписываем в формат string
 	}
 	return newString
 }
