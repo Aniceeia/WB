0456795f04a1d20345906bec59d31acf56d32100
daliatoo <daliatoo@student.21-school.ru>
2025-10-01 17:46:06 +0300

    add: distance finder

--- L1/L1.24/main.go
+++ L1/L1.24/main.go
@@ -19,14 +19,14 @@ func NewPoint(x, y float64) Point {
 func (p Point) Distance(other Point) float64 {
 	newX := p.x - other.x
 	newY := p.y - other.y
-	return math.Sqrt(newX*newX + newY*newY)
+	return math.Sqrt(newX*newX + newY*newY) //ищем по формуле расстояния между двумя точками в плоскости
 }
 
 func main() {
-	a1 := NewPoint(1.0, 2.0)
+	a1 := NewPoint(1.0, 2.0) //записываем в структуру Point товые точки
 	a2 := NewPoint(3.0, 4.0)
 
-	distance := a1.Distance(a2)
+	distance := a1.Distance(a2) //рассчитываем расстояние между a1 и a2
 
 	fmt.Printf("distance: %.f", distance)
 }
